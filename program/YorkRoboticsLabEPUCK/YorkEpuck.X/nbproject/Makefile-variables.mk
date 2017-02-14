f431ca207d87e92f4b7b8a556ac79e5ffbd4856c
Len Newbrook <lennewbrook@nas10-240-153-172.york.ac.uk>
2017-02-14 14:49:39 +0000

    First commit

---
+++ program/YorkRoboticsLabEPUCK/YorkEpuck.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=YorkEpuck.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/YorkEpuck.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=yorkepuck.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/yorkepuck.x.tar
